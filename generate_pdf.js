a2d3949ea7fe171e007cee3bc9a3eb0053fdfc25
Harsh Bangari <harshrock2010@gmail.com>
2023-10-19 17:26:30 +0530

    fonts added pdf content updated
    fonts added
    pdf content updated

--- generate_pdf.js
+++ generate_pdf.js
@@ -1,8 +1,15 @@
 const puppeteer = require("puppeteer");
 
 async function generatePDF() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+    '--no-sandbox',
+    '--disable-web-security'
+  ],
+  });
   const page = await browser.newPage();
+  // await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
+
 
   const filePath =
     "/Users/harshbangari/Documents/GitHub/pdf-creater-node/sample.html";
